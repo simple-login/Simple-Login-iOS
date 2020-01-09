a87deac27ac55a11749034c778d303999239d1cf
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-01-09 18:26:11 +0100

    Init empty project

---
+++ Simple Login/Simple Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Login
+//
+//  Created by Thanh-Nhon Nguyen on 09/01/2020.
+//  Copyright © 2020 SimpleLogin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
