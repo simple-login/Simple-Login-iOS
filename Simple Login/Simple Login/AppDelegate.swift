fb9afd162a606bf354337299fa12a2ddc5b372de
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-02-06 22:58:32 +0100

    Add firebase

--- Simple Login/Simple Login/AppDelegate.swift
+++ Simple Login/Simple Login/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Toaster
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setUpUI()
+        FirebaseApp.configure()
         return true
     }
     
