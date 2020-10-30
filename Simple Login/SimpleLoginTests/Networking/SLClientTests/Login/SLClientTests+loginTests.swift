c1c67af7b929e0c5b575d5bca5fc4ebfeda66127
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2020-10-30 14:29:06 +0100

    implement NetworkEngineMock

--- Simple Login/SimpleLoginTests/Networking/SLClientTests/Login/SLClientTests+loginTests.swift
+++ Simple Login/SimpleLoginTests/Networking/SLClientTests/Login/SLClientTests+loginTests.swift
@@ -10,26 +10,16 @@ import Foundation
 @testable import SimpleLogin
 import XCTest
 
-// swiftlint:disable nesting
-// swiftlint:disable force_unwrapping
-// swiftlint:disable force_try
 extension SLClientTests {
     func testLoginSuccessWithStatusCode200() throws {
         // given
-        class NetworkEngineMock: NetworkEngine {
-            func performRequest(for urlRequest: URLRequest, completionHandler: @escaping Handler) {
-                let response =
-                    HTTPURLResponse(url: urlRequest.url!, statusCode: 200, httpVersion: nil, headerFields: nil)
-                let data = try! XCTUnwrap(Data.fromJson(fileName: "POST_login_Response"))
-                completionHandler(data, response, nil)
-            }
-        }
+        let data = try XCTUnwrap(Data.fromJson(fileName: "POST_login_Response"))
+        let engine = NetworkEngineMock(data: data, statusCode: 200)
 
         // when
         var storedUserLogin: UserLogin?
         var storedError: SLError?
 
-        let engine = NetworkEngineMock()
         let sut = try SLClient(engine: engine)
         sut.login(email: "", password: "", deviceName: "") { result in
             switch result {
@@ -44,20 +34,13 @@ extension SLClientTests {
 
     func testLoginFailureWithStatusCode200() throws {
         // given
-        class NetworkEngineMock: NetworkEngine {
-            func performRequest(for urlRequest: URLRequest, completionHandler: @escaping Handler) {
-                let response =
-                    HTTPURLResponse(url: urlRequest.url!, statusCode: 200, httpVersion: nil, headerFields: nil)
-                let data = try! XCTUnwrap(Data.fromJson(fileName: "POST_login_MissingValueResponse"))
-                completionHandler(data, response, nil)
-            }
-        }
+        let data = try XCTUnwrap(Data.fromJson(fileName: "POST_login_MissingValueResponse"))
+        let engine = NetworkEngineMock(data: data, statusCode: 200)
 
         // when
         var storedUserLogin: UserLogin?
         var storedError: SLError?
 
-        let engine = NetworkEngineMock()
         let sut = try SLClient(engine: engine)
         sut.login(email: "", password: "", deviceName: "") { result in
             switch result {
