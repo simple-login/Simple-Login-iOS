2b608099f4119ea8a89afc01e38c831a683178f8
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-03-10 21:49:11 +0100

    [#9] Update custom domain model

--- Simple Login/Simple Login/Models/CustomDomain.swift
+++ Simple Login/Simple Login/Models/CustomDomain.swift
@@ -11,10 +11,14 @@ import UIKit
 
 final class CustomDomain: Decodable {
     let id: Int
-    let name: String
+    let name: String?
+    let domainName: String
+    let creationDate: String
     let creationTimestamp: TimeInterval
     let aliasCount: Int
     let isVerified: Bool
+    let catchAll: Bool
+    let randomPrefixGeneration: Bool
 
     lazy var countAttributedString: NSAttributedString = {
         var plainString = ""
@@ -49,7 +53,7 @@ final class CustomDomain: Decodable {
     lazy var catchAllDescriptionString: NSAttributedString = {
         // swiftlint:disable line_length
         let text = """
-            This feature allows you to create aliases on the fly. Simply use anything@\(name) next time you need an email address.
+            This feature allows you to create aliases on the fly. Simply use anything@\(domainName) next time you need an email address.
             The alias will be created the first time it receives an email.
             """
         // swiftlint:enable line_length
@@ -65,7 +69,7 @@ final class CustomDomain: Decodable {
                                           range: NSRange(onTheFlyRange, in: text))
         }
 
-        if let emailRange = text.range(of: "anything@\(name)") {
+        if let emailRange = text.range(of: "anything@\(domainName)") {
             attributedString.addAttribute(.backgroundColor,
                                           value: UIColor.systemYellow,
                                           range: NSRange(emailRange, in: text))
@@ -79,19 +83,27 @@ final class CustomDomain: Decodable {
 
     // swiftlint:disable type_name
     private enum Key: String, CodingKey {
-        case id = "id"
-        case name = "domain"
+        case catchAll = "catch_all"
+        case creatioDate = "creation_date"
         case creationTimestamp = "creation_timestamp"
+        case domainName = "domain_name"
+        case id = "id"
+        case isVerified = "is_verified"
+        case name = "name"
         case aliasCount = "nb_alias"
-        case isVerified = "verified"
+        case randomPrefixGeneration = "random_prefix_generation"
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: Key.self)
         self.id = try container.decode(Int.self, forKey: .id)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.creationTimestamp = Date().timeIntervalSince1970
+        self.name = try container.decode(String?.self, forKey: .name)
+        self.domainName = try container.decode(String.self, forKey: .domainName)
+        self.creationDate = try container.decode(String.self, forKey: .creatioDate)
+        self.creationTimestamp = try container.decode(TimeInterval.self, forKey: .creationTimestamp)
         self.aliasCount = try container.decode(Int.self, forKey: .aliasCount)
         self.isVerified = try container.decode(Bool.self, forKey: .isVerified)
+        self.catchAll = try container.decode(Bool.self, forKey: .catchAll)
+        self.randomPrefixGeneration = try container.decode(Bool.self, forKey: .randomPrefixGeneration)
     }
 }
