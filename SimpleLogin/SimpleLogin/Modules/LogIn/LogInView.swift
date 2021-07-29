ec93f2d1fa0d792a2a2f63b9517f6c4cd21c351f
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-07-29 23:14:12 +0200

    Init SwiftUI project

---
+++ SimpleLogin/SimpleLogin/Modules/LogIn/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  SimpleLogin
+//
+//  Created by Thanh-Nhon Nguyen on 29/07/2021.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
---
+++ SimpleLogin/SimpleLogin/SimpleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginApp.swift
+//  SimpleLogin
+//
+//  Created by Thanh-Nhon Nguyen on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogInView()
+        }
+    }
+}
