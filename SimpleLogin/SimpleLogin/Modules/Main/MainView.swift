fcb483f04c3769577579117d5d932c389987fe17
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-04-07 21:55:52 +0200

    [#69] Replace deprecated SKStoreReviewController.requestReview()

--- SimpleLogin/SimpleLogin/Modules/Main/MainView.swift
+++ SimpleLogin/SimpleLogin/Modules/Main/MainView.swift
@@ -115,7 +115,11 @@ struct MainView: View {
                     onCreateAlias: { createdAlias in
                         aliasCreationCount += 1
                         if launchCount >= 10, aliasCreationCount >= 5 {
-                            SKStoreReviewController.requestReview()
+                            if let scene = UIApplication.shared
+                                .connectedScenes
+                                .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                                SKStoreReviewController.requestReview(in: scene)
+                            }
                         }
                         self.createdAlias = createdAlias
                     },
